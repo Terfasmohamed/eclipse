1d43819b5c49c0b01e09a7e1f81264354796f888
AbdouPrognoz <oa_benalia@esi.dz>
2025-04-19 01:27:32 +0100

    ver1

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const express  = require('express');
+const app = express() ;
+const port = 3000;
+const mongoose = require('mongoose');
+
+
+app.use (express.json());
+mongoose.connect('mongodb://localhost:27017/mydb')
+  .then(() => console.log("Connected to DB!"))
+  .catch(err => console.log("DB connection failed :(", err));
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
